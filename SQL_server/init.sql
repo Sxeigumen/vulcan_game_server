2ab4beb982d249fd13535ce17c8c9d93c0b1c473
Kyala16 <artem.myakishev.2018@mail.ru>
2023-07-05 16:36:06 +0300

    sql_complete

---
+++ SQL_server/init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Controllers(
+                id serial PRIMARY KEY,
+                ip varchar(20) NOT NULL UNIQUE,
+                connection boolean);
+
+CREATE TABLE Smartphones(
+                id serial PRIMARY KEY,
+                ip varchar(20) NOT NULL,
+                connection boolean);
+
+CREATE TABLE Pairs(
+                id serial PRIMARY KEY,
+                id_controller int REFERENCES Controllers(id),
+                id_smartphone int REFERENCES Smartphones(id));
+
